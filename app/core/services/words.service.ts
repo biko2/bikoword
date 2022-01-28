ee802701c0141e5f5636d4adc25c568e6a9b8455
Enrique Arbunies <enrique.arbunies@biko2.com>
2022-01-28 17:31:13 +0100

    obtain the word of the day

---
+++ app/core/services/words.service.ts
@@ -0,0 +1,25 @@
+import { app } from "../infrastructure/firebase";
+import { child, get, getDatabase, ref } from "firebase/database";
+
+const getWords = async (): string[] => {
+  const databaseRef = ref(getDatabase(app));
+
+  const snapshot = await get(child(databaseRef, "palabras"));
+
+  return snapshot.val();
+};
+
+const getWordOfDay = async (): string => {
+  const words = await getWords();
+  // January 1, 2022 Game Epoch
+  const startDate = new Date("January 1, 2022 00:00:00").valueOf();
+  const now = Date.now();
+  const msInDay = 86400000;
+  const index = Math.floor((now - startDate) / msInDay);
+
+  return words[index % words.length].toUpperCase();
+};
+
+export const wordsService = {
+  getWordOfDay,
+};
--- app/routes/play/index.tsx
+++ app/routes/play/index.tsx
@@ -1,12 +1,21 @@
+import { useLoaderData } from "remix";
 import { Grid, links as GridLinks } from "~/components/Grid";
 import { Header } from "~/components/Header";
 import { Keyboard, links as KeyboardLinks } from "~/components/Keyboard";
+import { wordsService } from "~/core/services/words.service";
 
 export function links() {
   return [...GridLinks(), ...KeyboardLinks()];
 }
 
-const Play = () => {
+export async function loader() {
+  return await wordsService.getWordOfDay();
+}
+
+export default function Play() {
+  const wordOfDay = useLoaderData();
+  console.log(wordOfDay);
+
   return (
     <>
       <Header />
@@ -14,6 +23,4 @@ const Play = () => {
       <Keyboard />
     </>
   );
-};
-
-export default Play;
+}
