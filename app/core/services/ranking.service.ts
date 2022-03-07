1b7c6932e19fe23711d39100d1b05a38ed7ff6f2
Enrique Arbunies <enrique.arbunies@biko2.com>
2022-03-07 16:11:49 +0100

    modify user name

--- app/core/services/ranking.service.ts
+++ app/core/services/ranking.service.ts
@@ -12,15 +12,32 @@ const getPersonalScore = async (user: User): Promise<number> => {
   return userScore?.score ?? 0;
 };
 
+const getProcessedName = (name: string): string => {
+  const splitedName = name.split(" ");
+  const usefulSplits = splitedName.slice(0, 2);
+
+  if (!usefulSplits[1]) {
+    return usefulSplits[0];
+  }
+
+  usefulSplits[1] = usefulSplits[1].charAt(0);
+
+  const processedName = usefulSplits.join(" ");
+
+  return processedName;
+};
+
 const setPersonalScore = async (user: User, points: number) => {
   const databaseRef = ref(getDatabase(app));
 
   const previousScore = await getPersonalScore(user);
 
+  const nameToShow = getProcessedName(user.displayName);
+
   await set(child(databaseRef, `ranking/${user?.id}`), {
     id: user.googleId,
     email: user.email,
-    name: user.displayName,
+    name: nameToShow,
     photo: user.displayPhoto,
     score: previousScore + points,
   });
