21d7f91b89987c047fba1ddb204e31e3be17e0cb
josu-alzueta <82585112+josu-alzueta@users.noreply.github.com>
2022-03-07 09:13:55 +0100

    Added home rules modal (#15)
    * Added home rules modal
    
    * Added useState type
    
    Co-authored-by: MrAlzu <josualzueta@gmail.com>

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,16 @@
-import { Link } from "remix";
+import { useState } from "react";
 import { useUser } from "~/hooks/useUser";
+import { RulesModal, links as RulesModalLinks } from "~/components/RulesModal";
+import { loginService } from "~/core/services/login.service";
+import { NavLink } from "remix";
+
+export function links() {
+  return [...RulesModalLinks()];
+}
 
 export default function Index() {
   const { user, logout } = useUser();
+  const [isRulesModalOpen, setRulesModalOpen] = useState<boolean>(false);
 
   const logOut = () => {
     logout();
@@ -10,23 +18,45 @@ export default function Index() {
 
   return (
     <nav className="router">
-      <p>
-        Bienvenido a <strong>BikoWordle</strong>,<br />
-        ¡La nueva app de Biko customizada para jugar al Wordle!
-      </p>
-      <p>
-        Si quieres jugar, descubre
-        <Link to="/play" className="inner-link custom-link">
-          la palabra del día
-        </Link>
-      </p>
-      <p>
-        Si quieres checkear tu posición, visita
-        <Link to="/ranking" className="inner-link custom-link">
-          el ranking
-        </Link>
-      </p>
-      {user && <button onClick={logOut}>Cerrar sesión</button>}
+      <div className="welcome">
+        <p>
+          Bienvenido a <strong>BikoWordle</strong>,<br />
+          ¡La nueva app de Biko customizada para jugar al Wordle!
+        </p>
+        <p>
+          Si no tienes mucha idea de cómo funciona el juego, haz{" "}
+          <strong
+            className="inner-link custom-link"
+            onClick={() => setRulesModalOpen(true)}
+          >
+            click aquí
+          </strong>
+        </p>
+        <p>Pero si lo tienes claro...</p>
+
+        <div className="ctas-wrapper">
+          {user && (
+            <>
+              <NavLink to="/play" className="header-item">
+                <button className="button primary">¡A JUGAR!</button>
+              </NavLink>
+              <button className="button secondary" onClick={logOut}>
+                Cerrar sesión
+              </button>
+            </>
+          )}
+          {!user && (
+            <button className="button primary" onClick={loginService.signInApp}>
+              Inicia sesión
+            </button>
+          )}
+        </div>
+
+        <RulesModal
+          isOpen={isRulesModalOpen}
+          onClose={() => setRulesModalOpen(false)}
+        />
+      </div>
     </nav>
   );
 }
